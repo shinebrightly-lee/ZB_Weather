9f375788f62a01aa42adf677789b89752ce1d18c
MH <gmhmhmhg.dev@gmail.com>
2023-05-29 21:44:04 +0900

    트랜잭션

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,13 +2,11 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Scanner;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class WeatherApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
